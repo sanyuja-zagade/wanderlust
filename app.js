cf8711e26004b5522815c7729743b18c29a11f53
Sanyuja Zagade <sanyujaz6205@gmail.com>
2025-11-24 12:35:52 +0530

    Resolved currentUser problem

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.engine("ejs", ejsMate);
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
-    res.locals.currentUser = req.user;
+    res.locals.currentUser = req.user || null;
     next();
 });
 
